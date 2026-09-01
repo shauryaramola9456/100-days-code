15a2dd3a393ed00eb6e6dd673f44dfa4a347273f
Shaurya Ramola <shauryaramola56@gmail.com>
2026-09-01 23:02:03 +0530

    Refactor series sum program for clarity and formatting

--- 100 days code/Day23_1.c
+++ 100 days code/Day23_1.c
@@ -1,4 +1,4 @@
-//Q45:Write a program to find the sum of the series: 2/3 + 4/7 + 6/11 + 8/15 + ... up to n terms.
+//Q45: Write a program to find the sum of the series: 2/3 + 4/7 + 6/11 + 8/15 + ... up to n terms.
 #include<stdio.h>
 int main()
 {
