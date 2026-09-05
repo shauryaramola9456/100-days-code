1cb95f1ec7cecb4373909c1aab42ba0266e516bc
Shaurya Ramola <shauryaramola56@gmail.com>
2026-09-05 21:54:22 +0530

    Add files via upload

---
+++ 100 days code/Day27_1.c
@@ -0,0 +1,25 @@
+//Q53: Write a program to print the following pattern:
+#include<stdio.h>
+int main()
+{
+    int i, j;
+    // Print the upper half
+    for(i = 1; i <= 5; i++)
+    {
+        for(j = 1; j <= 2 * i - 1; j++)
+        {
+            printf("*");
+        }
+        printf("\n");
+    }
+    // Print the lower half
+    for(i = 4; i >= 1; i--)
+    {
+        for(j = 1; j <= 2 * i - 1; j++)
+        {
+            printf("*");
+        }
+        printf("\n");
+    }
+    return 0;
+}
