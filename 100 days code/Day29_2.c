11b52f5b197bb758c25edf628f1e49a25aea8696
Shaurya Ramola <shauryaramola56@gmail.com>
2026-09-07 22:56:24 +0530

    Add files via upload

---
+++ 100 days code/Day29_2.c
@@ -0,0 +1,22 @@
+// Q58 Find the maximum and minimum element in an array.
+#include<stdio.h>
+
+int main()
+{
+    int a[100], n, i, sum = 0;
+
+    printf("Enter the number of elements: ");
+    scanf("%d", &n);
+
+    printf("Enter %d elements:\n", n);
+
+    for(i = 0; i < n; i++)
+    {
+        scanf("%d", &a[i]);
+        sum = sum + a[i];
+    }
+
+    printf("Sum of array elements = %d\n", sum);
+
+    return 0;
+}
