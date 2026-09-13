ba3bec5bcd162b86f324227fbec26088dbdcfa92
Shaurya Ramola <shauryaramola56@gmail.com>
2026-09-13 22:41:18 +0530

    Add files via upload

---
+++ 100 days code/Day35_1.c
@@ -0,0 +1,44 @@
+//Q69:Find the second largest element in an array.
+#include <stdio.h>
+
+int main()
+{
+    int a[100], n, i;
+    int largest, second;
+
+    printf("Enter the number of elements: ");
+    scanf("%d", &n);
+
+    printf("Enter the array elements:\n");
+    for(i = 0; i < n; i++)
+    {
+        scanf("%d", &a[i]);
+    }
+
+    largest = a[0];
+    second = a[1];
+
+    if(second > largest)
+    {
+        int temp = largest;
+        largest = second;
+        second = temp;
+    }
+
+    for(i = 2; i < n; i++)
+    {
+        if(a[i] > largest)
+        {
+            second = largest;
+            largest = a[i];
+        }
+        else if(a[i] > second && a[i] != largest)
+        {
+            second = a[i];
+        }
+    }
+
+    printf("Second largest element = %d", second);
+
+    return 0;
+}
---
+++ 100 days code/Day35_2.c
@@ -0,0 +1,42 @@
+//Q70: Rotate an array to the right by k positions.
+#include <stdio.h>
+
+int main()
+{
+    int a[100], n, k, i, j, last;
+
+    printf("Enter the number of elements: ");
+    scanf("%d", &n);
+
+    printf("Enter the array elements:\n");
+    for(i = 0; i < n; i++)
+    {
+        scanf("%d", &a[i]);
+    }
+
+    printf("Enter the number of positions to rotate: ");
+    scanf("%d", &k);
+
+    k = k % n;
+
+    for(i = 0; i < k; i++)
+    {
+        last = a[n - 1];
+
+        for(j = n - 1; j > 0; j--)
+        {
+            a[j] = a[j - 1];
+        }
+
+        a[0] = last;
+    }
+
+    printf("Array after right rotation:\n");
+
+    for(i = 0; i < n; i++)
+    {
+        printf("%d ", a[i]);
+    }
+
+    return 0;
+}
